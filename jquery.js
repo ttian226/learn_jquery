bf6416f37c2c2863a3ee051c3ca4225485befcab
wangxu <ttian226@gmail.com>
2015-05-07 16:56:58 +0800

    update

--- jquery.js
+++ jquery.js
@@ -14,12 +14,41 @@
 		}
 	};
 
-	//静态方法
-	jQuery.each = function (obj, callback, args) {
+	jQuery.fn.init.prototype = jQuery.fn;
 
+	//jQuery插件扩展
+	jQuery.extend = jQuery.fn.extend = function () {
+		var options, name, copy,
+			target = arguments[0] || {},
+			i = 1,
+			length = arguments.length;
+
+		//只有一个参数，就是对jQuery或jQuery原型对象的扩展
+		if (i === length) {
+			//调用的上下文对象jQuery或jQuery原型对象
+			target = this;
+			i--;
+		}
+
+		for (; i < length; i++) {
+			if ((options = arguments[i]) != null) {
+				//options不为空时开始遍历
+				for (name in options) {
+					copy = options[name];
+					//扩展jQuery对象或原型对象
+					target[name] = copy;
+				}
+			}
+		}
+		return target;
 	};
 
-	jQuery.fn.init.prototype = jQuery.fn;
+	//静态方法
+	jQuery.extend({
+		each: function (obj, callback, args) {
+			//...code...
+		}
+	});
 
 	window.jQuery = window.$ = jQuery;
 }(window));
