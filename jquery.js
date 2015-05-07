6e3da6e310ead9e4b3db6c97b45174f7e009a6ae
wangxu <ttian226@gmail.com>
2015-05-07 15:02:21 +0800

    update

--- jquery.js
+++ jquery.js
@@ -1,9 +1,25 @@
 (function (window, undefined) {
 	var jQuery = function (selector) {
-		if (!(this instanceof jQuery)) {
-			return new jQuery(selector);
-		}
-		this.selector = selector;
+		return new jQuery.fn.init(selector);
 	};
+
+	jQuery.fn = jQuery.prototype = {
+		constructor: jQuery,
+		init: function (selector) {
+			return this;
+		},
+		//实例方法
+		each: function (callback, args) {
+			return jQuery.each(this, callback, args);
+		}
+	};
+
+	//静态方法
+	jQuery.each = function (obj, callback, args) {
+
+	};
+
+	jQuery.fn.init.prototype = jQuery.fn;
+
 	window.jQuery = window.$ = jQuery;
 }(window));
